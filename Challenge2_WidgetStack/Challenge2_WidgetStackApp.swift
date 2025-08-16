4a87b9f35dc50eafd5af2bbf0ecc0f2f53e61584
Mohammed Hashim Nazeer Ahamed <mohammed.hashim.nazeer.ahamed@sji.edu.sg>
2025-08-16 11:06:26 +0800

    Initial Commit

---
+++ Challenge2_WidgetStack/Challenge2_WidgetStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge2_WidgetStackApp.swift
+//  Challenge2_WidgetStack
+//
+//  Created by T Krobot on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge2_WidgetStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
